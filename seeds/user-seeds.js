c5c767d668fa1a7cbf2756ddb270de314bdb64cc
fanidt <fanidtu@gmail.com>
2021-07-30 01:07:06 -0700

    seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,33 +3,31 @@ const { User } = require('../models');
 const userData = [
   {
     username: "Cassie",
-    email: "cassie@gmail.com",
+    email: "cassie@mail.com",
     password: "password1234"
   },
   {
     username: "Josh",
-    email: "josh@gmail.com",
+    email: "josh@mail.com",
     password: "password1234"
   },
   {
     username: "Steven",
-    email: "steven@gmail.com",
+    email: "steven@mail.com",
     password: "password1234"
   },
   {
     username: "Emily",
-    email: "emily@gmail.com",
+    email: "emily@mail.com",
     password: "password1234"
   },
   {
     username: "John",
-    email: "john@gmail.com",
+    email: "john@mail.com",
     password: "password1234"
   }
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
-
-//  WARNING seed bulk create does NOT hash the password, so they must be hashed via the update route before the login route will work!
 
 module.exports = seedUsers;
