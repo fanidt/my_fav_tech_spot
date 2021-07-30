79cf023e9e1a404fb15b733c5602f54e6d8126ab
fanidt <fanidtu@gmail.com>
2021-07-29 23:43:48 -0700

    retry fixing it since i destroyed it

--- models/index.js
+++ models/index.js
@@ -13,25 +13,25 @@ Post.belongsTo(User, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'null',
+    onDelete: 'cascade',
     hooks: true
 });
 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
-    onDelete: 'null',
+    onDelete: 'cascade',
     hooks: true
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
-    onDelete: 'null',
+    onDelete: 'cascade',
     hooks: true
 });
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
-    onDelete: 'null',
+    onDelete: 'cascade',
     hooks: true
 })
 
